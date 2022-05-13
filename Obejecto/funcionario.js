7e9b1c239f4706fd7c39d3e4db6cedcbc2d6f7f2
Gledse_Jamisse <53790917+Gledse@users.noreply.github.com>
2022-05-13 22:24:13 +0200

    commit

--- Obejecto/funcionario.js
+++ Obejecto/funcionario.js
@@ -1,4 +1,60 @@
-function Funcionario() {
+// Programação orientada a objectos
+class Funcionario {
+
+    constructor() { // metodo construtor >>> primeira função que é chamado toda vez que nós executa a nossa classe
+        this.codigo = 0;
+        this.nome = '';
+        this.sexo = ''
+    }
+    adicionar() {
+        window.alert('Funcionou!')
+    }
+
+    visualizar() {
+        window.alert('item visualizado com sucesso!')
+    }
+
+    apagar() {
+        window.alert('item excluido com sucesso!')
+    }
+}
+ var funcionario = new Funcionario(); // instanciar
+
+
+/*
+Obecto
+    classe funcionario
+        Atributos(id, nome, valor)
+
+        Metodos (funções, adicionar(), deletar(), editar())
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function Funcionario() {
     this.codigo = codigo
     this.nome = nome
     this.sexo = sexo
@@ -9,7 +65,7 @@ function cadastrar() {
     f.nome = document.querySelector('input#txtNome')
     let sex = document.getElementsByName('radSexo')
     f.codigo = Number(cod.value)
-    
+
     let genero = ''
     if (sex[0].checked) {
         genero = 'Masculino'
@@ -29,4 +85,4 @@ function pesquisar() {
     res.innerHTML += `Código: ${f.codigo} <br>`
     res.innerHTML += `Nome: ${f.nome} <br>`
     res.innerHTML += `Sexo: ${f.sexo} <br>`
-}
+}*/
